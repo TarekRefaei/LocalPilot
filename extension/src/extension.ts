fcfc681bddc71d1fc03b15c6a4ec5897008f1720
Tarek Mohammed Refaie <refa3igroup@gmail.com>
2025-11-17 21:11:03 +0200

    chore(ci): quality gates + caching; strict TS/ESLint; Black; VS Code tasks; templates

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -2,9 +2,7 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   const cmd = vscode.commands.registerCommand('localpilot.helloWorld', () => {
-    vscode.window.showInformationMessage('LocalPilot ready');
+    void vscode.window.showInformationMessage('LocalPilot ready');
   });
   context.subscriptions.push(cmd);
 }
-
-export function deactivate() {}
