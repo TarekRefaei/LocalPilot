b04a093ddb45e13c029dc4419104549a4284bb24
Tarek Mohammed Refaie <refa3igroup@gmail.com>
2025-12-02 21:26:33 +0200

    test(extension): import mocha globals in integration test suite to fix Windows run

--- extension/test/suite/index.js
+++ extension/test/suite/index.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const vscode = require('vscode');
+const { describe, it } = require('mocha');
 
 describe('Extension Test Suite', () => {
   it('Open custom LocalPilot webview', async () => {
